4fc71ad835407d72bd9d6a2260815afef871d6c1
Pablo <pablo.garavito@gmail.com>
2026-03-13 18:54:42 -0500

    added local profile (never to be committed) with db credentials, first real commit

---
+++ backend/src/main/java/com/pablogb/psycho_logger/PsychoLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.pablogb.psycho_logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychoLoggerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pablogb/psycho_logger/PsychoLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablogb.psycho_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychoLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
